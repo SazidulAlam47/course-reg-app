6f4adddb9a52279a84fcb835b783a4bfb603fcd4
Sazidul Alam <sazidulalam20@gmail.com>
2023-09-18 19:27:25 +0600

    purchase tost fixed

--- src/components/Tost/Tost.jsx
+++ src/components/Tost/Tost.jsx
@@ -1,11 +1,17 @@
 import './Tost.css';
 
 const Tost = ({tost, setTost, tostMessage}) => {
+    const handelOk = () => {
+        setTost(false);
+        if (tostMessage === 'Thanks for your purchase') {
+            window.location.reload();
+        }
+    }
     if (tost)
         return (
             <div className='tost'>
                 <span>{tostMessage}</span>
-                <button onClick={() => setTost(false)}>Ok</button>
+                <button onClick={handelOk}>Ok</button>
             </div>
         );
 };
